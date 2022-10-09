2f57218cd37f714f687f0b6291e70c96fcd76a17
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2022-10-09 20:53:12 +0300

    Update 8-condense-array-to-number.js

--- Arrays-Lab/8-condense-array-to-number.js
+++ Arrays-Lab/8-condense-array-to-number.js
@@ -1,7 +1,6 @@
 function solve(array) {
 
     while (array.length > 1){
-        array2 = [array.length - 1];
         
         for (let i = 0; i < array.length - 1; i++){
             array[i] = array[i] + array[i + 1];
