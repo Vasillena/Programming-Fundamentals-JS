10f73f407809a9e042be40c5aa28523ba02e9e34
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2022-10-30 12:50:59 +0200

    Delete Arrays-Lab directory

--- Arrays-Lab/1-sum-first-and-last-array-elements.js
+++
@@ -1,9 +0,0 @@
-function solve(numbers) {
-    let firstElement = numbers[0];
-    let lastElement = numbers[numbers.length - 1];
-
-    let sum = firstElement + lastElement;
-    console.log(sum);
-}
-
-solve([20, 30, 40]);
