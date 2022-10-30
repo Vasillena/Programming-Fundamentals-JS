058aaf90f13d6f17759f36be3fe5d5ec670288e2
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2022-10-30 12:37:37 +0200

    Update 3-reverse-an-array.js

--- Arrays-Lab/3-reverse-an-array.js
+++ Arrays-Lab/3-reverse-an-array.js
@@ -1,8 +1,15 @@
 function reverse(count, numbers) {
-    let reversedNumbers = [];
+    // create new array
+    let reversedNumbers = []; // create empty array
+
+    // take count elements from numbers
+    // reverse the elements
     for (let i = 0; i < count; i++) {
+        // result[count - 1 - i] = numbers[i];
         reversedNumbers[i] = numbers[count - 1 - i];
     }
+
+    // print for winners :) 
     let result = '';
     for (let i = 0; i < reversedNumbers.length; i++) {
         result += reversedNumbers[i];
